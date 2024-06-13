b38c770a295b9352cd3c51c43af92c7793c67c97
Hector JS <58092446+JSHectorM@users.noreply.github.com>
2024-06-13 14:21:40 -0600

    add contenido login.component y form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,17 +1,54 @@
-import { Component } from '@angular/core';
-import {FormBuilder} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {faEye, faEyeSlash} from "@fortawesome/free-regular-svg-icons";
+import {faGoogle, faFacebook, faApple} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
-  constructor(private fb: FormBuilder) {
+  // Icons
+  faEye = faEye;
+  faEyeTSlash = faEyeSlash;
+  faGoogle = faGoogle;
+  faFacebook =  faFacebook;
+  faApple =  faApple;
 
+  // flag show pssw
+  showPassw = false;
+  formLogin !: FormGroup;
+  submited = false;
+
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.formLoginInit()
+  }
+
+  get fl(): { [key: string]: AbstractControl } {
+    return this.formLogin.controls;
+  }
+
+  formLoginInit(){
+    this.formLogin = this.fb.group({
+      user: ['', [Validators.required]],
+      pssw: ['', [Validators.required]],
+      remember : ['true',[]]
+    })
   }
 
 
+  login(){
+    this.submited = true;
+    console.log(this.formLogin.value)
+  }
 
+  changeShowPssw(){
+    this.showPassw = !this.showPassw;
+
+  }
 }
